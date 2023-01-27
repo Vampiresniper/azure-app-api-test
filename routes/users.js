709a177ed8c37c0c35ce37b94d86816af6559544
Kevin Kammel <kevin.kammel@googlemail.com>
2023-01-27 07:52:58 +0100

    get request for userid added (/users/{id})

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/:userid', function(req, res, next) {
+  res.send(`Request with UserId: ${req.params.userid}`);
+});
+
 module.exports = router;
