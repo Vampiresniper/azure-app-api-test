5c5c164870c7e311569ac66381958331af3834ee
Kevin Kammel <kevin.kammel@googlemail.com>
2023-01-27 09:02:23 +0100

    name route added

--- routes/users.js
+++ routes/users.js
@@ -10,4 +10,8 @@ router.get('/:userid', function(req, res, next) {
   res.send(`Request with UserId: ${req.params.userid}`);
 });
 
+router.get('/name/:name', function(req, res, next) {
+  res.send(`Request with Name: ${req.params.name}`);
+});
+
 module.exports = router;
